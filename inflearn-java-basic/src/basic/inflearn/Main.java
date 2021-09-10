e8e5e1c106e2244d3f2eac4c4a74de34a15984a6
hwany <trin_@naver.com>
2021-09-10 21:43:32 +0900

    008 ~ 010 문제풀이

--- inflearn-java-basic/src/basic/inflearn/Main.java
+++ inflearn-java-basic/src/basic/inflearn/Main.java
@@ -6,11 +6,50 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String a = "abc";
-        String b = "132";
 
-        String c = a + b;
-        System.out.println(c);
+        String a ="CDABQ";
+
+        char[] arr = a.toCharArray();
+        Arrays.sort(arr);
+
+        String b = new String(arr);
+
+        System.out.print('A'-'B');
+
+
+        ArrayList<String> list = new ArrayList<>();
+        list.clear();
+
 
     }
 }
+
+
+//    public int[][] rotation (int[][] arr) {
+//        int m = arr.length;
+//        int[][] _arr = new int[m][m];
+//
+//        for (int i=0; i<m; i++) {
+//            for (int j=0; j<m; j++) {
+//                _arr[i][j] = arr[m-1-j][i];
+//            }
+//        }
+//
+//        return _arr;
+//
+//        // for (int i=0; i<m; i++) {
+//        //     for (int j=0; j<m; j++) {
+//        //         switch (deg) {
+//        //             case 90:
+//        //                 _arr[i][j] = arr[m-1-j][i];
+//        //                 break;
+//        //             case 180:
+//        //                 _arr[i][j] = arr[m-1-i][m-1-j];
+//        //                 break;
+//        //             case 270:
+//        //                 _arr[i][j] = arr[j][m-1-i];
+//        //                 break;
+//        //         }
+//        //     }
+//        // }
+//    }
