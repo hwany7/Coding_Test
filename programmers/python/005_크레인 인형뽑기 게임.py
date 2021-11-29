0f0ddb1c1c6232ec7416585bd7e4a5d9bf3bffea
hwany <trin_@naver.com>
2021-11-29 17:48:21 +0900

    005_크레인 인형뽑기 게임

---
+++ programmers/python/005_크레인 인형뽑기 게임.py
@@ -0,0 +1,19 @@
+import collections
+
+def solution(board, moves):
+    cnt = 0
+    dq = collections.deque()
+
+    for m in moves:
+        for i in range(len(board)):
+            if board[i][m-1] != 0:
+                dq.append(board[i][m-1])
+                board[i][m-1] = 0
+
+                while len(dq)>=2 and dq[-1] == dq[-2]:
+                    print(dq)
+                    dq.pop(), dq.pop()
+                    cnt += 1
+                break
+
+    return cnt * 2
